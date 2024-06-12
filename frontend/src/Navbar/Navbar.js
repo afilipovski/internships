f4cffa6e8fb93d5b5cc85a621e04292e78582a3c
Luka Krstikj <luka.krstik@protonmail.com>
2024-06-12 19:01:19 +0200

    text size change and responsivness fix on logo in navbar

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -6,11 +6,13 @@ import Pfp from '../assets/Pfp.jpg'
 import LogoutSVG from '../assets/logout.svg'
 
 const Navbar = () => (
-  <div className="Navbar" class="h-[8vh] p-4 flex justify-between px-2 lg:px-16 shadow-xl w-full overflow-hidden">
-      <img class="h-[5vh]" src={Logo}/>
+  <div className="Navbar" class="h-[7vh] p-4 flex justify-between px-2 lg:px-16 shadow-xl w-full overflow-hidden">
+      <div class="flex flex-col justify-center">
+        <img class="h-[4vh]" src={Logo}/>
+      </div>
       <div class="flex">
           <div class="flex flex-col justify-center">
-              <div class="flex rounded-xl border-2 border-black w-fit h-fit text-xl">
+              <div class="flex rounded-xl border-2 border-black w-fit h-fit text-lg">
                   <div class="flex p-2">
                       <h1 class="font-bold mx-2 h-fit w-fit">Лука Крстиќ</h1>
                       <h1 className="mx-2 h-fit w-fit">(213257)</h1>
