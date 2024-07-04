4aa2790d626c6917ad4128aaee7f868f469e46e6
DimitrijKrstev <antishadowss@gmail.com>
2024-07-05 00:09:56 +0200

    cascade clean.sql script

--- backend/src/main/java/mk/ukim/finki/wp/internships/datainit/clean.sql
+++ backend/src/main/java/mk/ukim/finki/wp/internships/datainit/clean.sql
@@ -1,8 +1,8 @@
-DROP TABLE internship_week;
-DROP TABLE internship;
-DROP TABLE internship_coordinator;
-DROP TABLE internship_supervisor;
-DROP TABLE internship_posting;
+DROP TABLE internship_week CASCADE;
+DROP TABLE internship CASCADE;
+DROP TABLE internship_coordinator CASCADE;
+DROP TABLE internship_supervisor CASCADE;
+DROP TABLE internship_posting CASCADE;
 DELETE FROM auth_user WHERE role='STUDENT' or role='SUPERVISOR';
 DELETE FROM student WHERE student_index LIKE '%999';
 DELETE FROM company WHERE id='company1';
