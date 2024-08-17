71f5518cd66783bb942e4ed4fc96ab6fedeedc17
211047 <aleksandar.filipovski@students.finki.ukim.mk>
2024-08-17 13:34:26 +0200

    save basic auth token to localstorage

--- frontend/src/repository/HttpClient.js
+++ frontend/src/repository/HttpClient.js
@@ -4,16 +4,36 @@ let token = '';
 
 const setAuthToken = (newToken) => {
     token = newToken;
-    instance.defaults.headers.common['Authorization'] = `Basic ${token}`;
+    localStorage.setItem("basicAuthToken", `Basic ${token}`);
 };
 
-const instance = axios.create({
+const httpInstance = axios.create({
     baseURL: process.env.BACKEND_BASE_URL ?? 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
     }
 });
+
+const setAuthHeader = () => {
+    httpInstance.defaults.headers.common['Authorization'] = localStorage.getItem("basicAuthToken");
+};
+
+const request = async (method, ...args) => {
+    setAuthHeader();
+    return await httpInstance[method](...args);
+};
+
+const instance = {
+    instance: httpInstance,
+    get: (...args) => request('get', ...args),
+    post: (...args) => request('post', ...args),
+    put: (...args) => request('put', ...args),
+    delete: (...args) => request('delete', ...args),
+    patch: (...args) => request('patch', ...args),
+    head: (...args) => request('head', ...args),
+    options: (...args) => request('options', ...args),
+};
 
 export { instance, setAuthToken };
 export default instance;
